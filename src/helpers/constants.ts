1783287bf3abacd0494fe6c36ccc7a1b262082e2
Vladimir Gerasimov <freelancervip@gmail.com>
2020-12-17 17:40:55 +0600

    Reduced password min length (support short seed names and password)

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,5 +1,5 @@
 export const LOGIN_MIN_LENGTH = 3;
-export const PASSWORD_MIN_LENGTH = 6;
+export const PASSWORD_MIN_LENGTH = 3;
 export const JWT_ACCESS_TOKEN_EXPIRATION = 1000 * 60 * 15; //in ms; 15 mins
 export const JWT_REFRESH_TOKEN_EXPIRATION = 24 * 60 * 60 * 1000; //in ms; 24hr/1d
 export const JWT_REFRESH_TOKEN_COOKIE_NAME = 'rt';
